8d24b3d91d2fdbeb12cbe2dffca6784e20fa7f87
quchengyang <1240814059@qq.com>
2018-05-07 15:27:09 +0800

    pierre

---
+++ README.md
@@ -0,0 +1,3 @@
+### 这是一个初学websocket的小demo
+
+### [笔记心得分享地址][https://www.jianshu.com/p/4d1af9ca950f]
---
+++ server.js
@@ -0,0 +1,19 @@
+let ws = require('nodejs-websocket');
+let PORT = 3001;
+const server = ws.createServer((conn) => {
+    console.log("New connection");
+    conn.on("text", (str) => {
+        console.log("Received" + str);
+        conn.sendText(str.toUpperCase() + "!!!");
+    })
+    conn.on("close", (code, reason) => {
+        console.log("Connection closed");     
+    })
+    conn.on("error", (err) => {
+        console.log("handle err"); 
+        console.log(err);     
+    })
+    
+}).listen(PORT);
+
+console.log("websocket server listing on port: " + PORT);
